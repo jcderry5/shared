52572f410ce267d1e7369a53557a76d8e97beb24
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-29 14:00:20 -0700

    Editing main.py some more

--- main.py
+++ main.py
@@ -21,25 +21,32 @@ class HomePage(webapp2.RequestHandler):
 
         user = users.get_current_user()
         if user:
-          # The if block only runs if the user IS logged in.
-          # The user has a method called `nickname()` that looks up their email.
-          # We can use this information to show the user who they're logged in as.
+          # Create the sign out link (for later use).
+          signout_link_html = '<a href="%s">sign out</a>' % (users.create_logout_url('/'))
+          # If the user is logged in, get their email address.
           email_address = user.nickname()
-          # Generate a sign out link - this does it all in one line.
-          logout_link_html = '<a href="%s">sign out</a>' % (
-                                users.create_logout_url('/'))
-          # Show that sign out link on screen:
-          self.response.write(
-                "You're logged in as " + email_address + "<br>" + logout_link_html)
+          # Then query Datastore to see if a user with this email has registered as
+          # a CssiUser before.
+          cssi_user = CssiUser.query().filter(CssiUser.email == email_address).get()
+          # If the query is successful, the variable will have a user in it, so the
+          # following code will run.
+          if cssi_user:
+            self.response.write(
+              "Looks like you're registered. Thanks for using our site!")
+          # if the query wasn't successful, the variable will be empty, so this code
+          # will run instead.
+          else:
+            self.response.write(
+              "Looks like you aren't a CSSI User. Please sign up.")
         else:
-          # This line creates a URL to log in with your Google Credentials.
-          login_url = users.create_login_url('/')
-          # This line uses string templating to create an anchor (link) element.
-          login_html_element = '<a href="%s">Sign in</a>' % login_url
-          # This line puts that URL on screen in a clickable anchor elememt.
-          self.response.write('Please log in.<b>' + login_html_element)
+      # This line creates a URL to log in with your Google Credentials.
+      login_url = users.create_login_url('/')
+      # This line uses string templating to create an anchor (link) element.
+      login_html_element = '<a href="%s">Sign in</a>' % login_url
+      # This line puts that URL on screen in a clickable anchor elememt.
+      self.response.write('Please log in.<b>' + login_html_element)
 
-class AuralPage(webapp2.RequestHandler):
+class AuditoryPage(webapp2.RequestHandler):
     def get(self):
         quiz_template = jinja_env.get_template('templates/aural.html')
 
@@ -70,4 +77,6 @@ app = webapp2.WSGIApplication([
     ('/Visual', VisualPage),
     ('/Profile', ProfilePage),
     ('/seed-data', LoadDataHandler),
+    ('/Auditory', AuralPage),
+    ('/Writing', WritingPage)
 ], debug=True)
