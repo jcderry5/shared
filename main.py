21a847f5d8abcbff2078d9fa22553ff92c2fad41
cssi <cssi@cssis-MacBook-Pro-90.local>
2019-07-29 15:08:27 -0700

    added space for the api

--- main.py
+++ main.py
@@ -48,6 +48,17 @@ class HomePage(webapp2.RequestHandler):
 
 class AuditoryPage(webapp2.RequestHandler):
     def get(self):
+        khan= 'http://www.khanacademy.org/api/v1/topictree'
+
+
+
+
+
+
+
+
+
+
         quiz_template = jinja_env.get_template('templates/aural.html')
 
 class WritingPage(webapp2.RequestHandler):
