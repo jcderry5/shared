2330c356c01d24ad62d05ecf36e11cf6898282ba
cssi <cssi@cssis-MacBook-Pro-90.local>
2019-07-29 09:09:46 -0700

    staet

---
+++ main.py
@@ -0,0 +1,14 @@
+mport webapp2
+import jinja2
+import os
+
+
+the_jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        priny("KMA")
