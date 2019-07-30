336bd5f19dbb0066d9f81b25169217bf77a15a5a
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-29 17:48:18 -0700

    Fixed Links!! **MUST RUN IN LOCAL HOST**

--- main.py
+++ main.py
@@ -31,33 +31,6 @@ class HomePage(webapp2.RequestHandler):
           # a H2JUser before.
           h2j_user = H2JUser.query().filter(H2JUser.email == email_address).get()
           # If the query is successful, the variable will have a user in it, so the
-          # following code will run.
-<<<<<<< HEAD
-=======
-          if cssi_user:
-            self.response.write(
-              "Looks like you're registered. Thanks for using our site!")
-          # if the query wasn't successful, the variable will be empty, so this code
-          # will run instead.
-          else:
-            self.response.write(
-              "Looks like you aren't a CSSI User. Please sign up.")
-        else:
-      # This line creates a URL to log in with your Google Credentials.
-      login_url = users.create_login_url('/')
-      # This line uses string templating to create an anchor (link) element.
-      login_html_element = '<a href="%s">Sign in</a>' % login_url
-      # This line puts that URL on screen in a clickable anchor elememt.
-      self.response.write('Please log in.<b>' + login_html_element)
-
-class AuditoryPage(webapp2.RequestHandler):
-    def get(self):
-        khan= 'http://www.khanacademy.org/api/v1/topictree'
-        result = urlfetch.fetch(khan).content
-        result_as_json = json.loads(result)
-        url = result_as_json[0]['url']
-
->>>>>>> fb98441730ee2bff38ca016ee2393a44fc191b7c
 
           home_dict = {
               "h2j_user" : h2j_user
@@ -105,13 +78,14 @@ class LoadDataHandler(webapp2.RequestHandler):
     def get(self):
         seed_data()
 
-
 class AuditoryPage(webapp2.RequestHandler):
     def get(self):
+        aural_template = jinja_env.get_template('templates/aural.html')
         khan= 'http://www.khanacademy.org/api/v1/topictree'
         result = urlfetch.fetch(khan).content
+        result_as_json = json.loads(result)
+        url = result_as_json[0]['url']
 
-        quiz_template = jinja_env.get_template('templates/aural.html')
 
 class WritingPage(webapp2.RequestHandler):
     def get(self):
