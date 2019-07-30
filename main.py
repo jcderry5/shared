4db759cac5e00791d61a8f3d13535ca8c11f5743
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-30 10:36:33 -0700

    Quiz is working!!!

--- main.py
+++ main.py
@@ -64,11 +64,31 @@ class HomePage(webapp2.RequestHandler):
 class QuizPage(webapp2.RequestHandler):
     def post(self):
         quiz_template = jinja_env.get_template('templates/quiz.html')
-        que_1 = self.request.get("q-1")
-        que_2 = self.request.get("q-2")
-        que_3 = self.request.get("q-3")
-        que_4 = self.request.get("q-4")
-        que_5 = self.request.get("q-5")
+        v_learn = 0
+        a_learn = 0
+        r_learn = 0
+
+        all_ques = []
+        all_ques.append(self.request.get("q-1"))
+        all_ques.append(self.request.get("q-2"))
+        all_ques.append(self.request.get("q-3"))
+        all_ques.append(self.request.get("q-4"))
+        all_ques.append(self.request.get("q-5"))
+
+        for value in all_ques:
+            if value == "v":
+                v_learn += 1
+            elif value == "a":
+                a_learn += 1
+            elif value == "r":
+                r_learn += 1
+        if v_learn > a_learn and v_learn >r_learn:
+            # self.response.write(jinja_env.get_template('templates/visual.html').render())
+            return webapp2.redirect("/Visual")
+        elif a_learn > v_learn and a_learn > r_learn:
+            return webapp2.redirect("/Auditory")
+        elif r_learn > v_learn and r_learn > a_learn:
+            return webapp2.redirect("/Writing")
 
     def get(self):
         quiz_template = jinja_env.get_template('templates/quiz.html')
