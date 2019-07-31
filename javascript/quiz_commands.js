cdd87ec5bef816fd3a127db110dc87d35baa1452
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-31 15:57:22 -0700

    Added fonts and finished styling the quiz page

--- javascript/quiz_commands.js
+++ javascript/quiz_commands.js
@@ -1,10 +1,34 @@
-// const quiz_ans = document.querySelector("#Q1_A1");
-//
-// function clicker(e){
-//   console.log("You clicked something")
-//   quiz_ans.style.backgroundColor = "#ffffff";
-//   console.log(quiz_ans)
-//
-// }
-//
-// quiz_ans.addEventListener('click', clicker);
+questions = {
+  "q1" : ["q-1v","q-1a","q-1r"],
+  "q2" : ["q-2v","q-2a","q-2r"],
+  "q3" : ["q-3v","q-3a","q-3r"],
+  "q4" : ["q-4v","q-4a","q-4r"],
+  "q5" : ["q-5v","q-5a","q-5r"],
+}
+background_color = {
+  'v' : 'v_box',
+  'a' : 'a_box',
+  'r' : 'r_box',
+
+}
+function myFunction(question, answer, color)
+{
+    var answers = questions[question];
+    // the array of whichever question you are in the the dict: questions
+
+    for(var i = 0; i < answers.length; i++){
+      if (answers[i]==answer){
+        //if selected
+        document.getElementById(answers[i]).classList.add("selected");
+        // document.getElementById(answers[i]).classList.remove(background_color[color]);
+        console.log("Selected " + answers[i]);
+      }
+
+      else{
+        // document.getElementById(answers[i]).classList.add(background_color[color]);
+        document.getElementById(answers[i]).classList.remove("selected");
+        console.log("Unselected " + answers[i]);
+      }
+
+    }
+  }
