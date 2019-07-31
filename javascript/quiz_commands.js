32a3dbbd165347814251946225f524b75aded839
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-31 11:35:09 -0700

    Made a lot of edits on quiz.html

---
+++ javascript/quiz_commands.js
@@ -0,0 +1,10 @@
+// const quiz_ans = document.querySelector("#Q1_A1");
+//
+// function clicker(e){
+//   console.log("You clicked something")
+//   quiz_ans.style.backgroundColor = "#ffffff";
+//   console.log(quiz_ans)
+//
+// }
+//
+// quiz_ans.addEventListener('click', clicker);
